c50d2a31648c2f730a3a3e934e4f8cdce2c34b6e
Muntaha-Noor <muntahanoor648@gmail.com>
2024-11-25 10:55:33 +0500

    Initial commit

---
+++ src/app/api/external/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+const API_ENDPOINT = "https://jsonplaceholder.typicode.com/posts";
+
+export const GET = async () => {
+    const response = await fetch(API_ENDPOINT)
+
+    if(!response.ok){
+       return NextResponse.json({ success: false}, { status: 501})
+    }
+
+    const data = await response.json()
+
+    return NextResponse.json({ data: data}, { status: 200})
+};
